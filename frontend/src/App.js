74dd832fd270fbacdcbd367eaddb998752a7dfcb
RafaelGoulartB <rafagoulartb@gmail.com>
2020-03-25 15:37:17 -0300

    Add Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Routes from './routes'
+
+import './global.css'
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Routes />
   );
 }
 
