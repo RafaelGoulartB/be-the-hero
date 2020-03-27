98cb0f4ac004b2aeaa5c9a55df27619d62a4e047
RafaelGoulartB <rafagoulartb@gmail.com>
2020-03-27 11:11:21 -0300

    Adding Validation in backend

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,4 +1,6 @@
 const routes = require('express').Router()
+const { celebrate, Joi, errors, Segments } = require('celebrate');
+
 const OngController = require('./controllers/OngController')
 const IncidentsController = require('./controllers/IncidentsController')
 const ProfileController = require('./controllers/ProfileController')
@@ -6,17 +8,45 @@ const SessionController = require('./controllers/SessionController')
 
 routes.route('/sessions')
   .post(SessionController.create)
-
+// Ongs
 routes.route('/ongs')
   .get(OngController.index)
-  .post(OngController.create)
+  .post(celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.string().required(),
+      city: Joi.string().required(),
+      uf: Joi.string().required().length(2),
+    })
+  }), OngController.create)
 
+// Incidents
 routes.route('/incidents')
-  .get(IncidentsController.index)
-  .post(IncidentsController.create)
-routes.route('/incidents/:id')
-  .delete(IncidentsController.delete)
+  .get(celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+      page: Joi.number()
+    })
+  }), IncidentsController.index)
+  .post(celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      title: Joi.string().required(),
+      description: Joi.string().required(),
+      value: Joi.number().required()
+    })
+  }), IncidentsController.create)
 
-routes.get('/profile', ProfileController.index)
+routes.delete('/incidents/:id', celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.number().required()
+  })
+}) ,IncidentsController.delete)
+
+// Profile
+routes.get('/profile', celebrate({
+  [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required()
+  }).unknown(),
+}), ProfileController.index)
 
 module.exports = routes
